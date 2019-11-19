ddb319dd8f645cc8a610ce684e9e912bc3110424
Difegue <sugoi@cock.li>
2019-11-19 20:58:52 +0100

    Design WSL error window

--- Setup/CustomDialog.Designer.cs
+++ Setup/CustomDialog.Designer.cs
@@ -26,67 +26,128 @@
     /// </summary>
     private void InitializeComponent()
     {
-        this.cancelBtn = new System.Windows.Forms.Button();
-        this.nextBtn = new System.Windows.Forms.Button();
-        this.backBtn = new System.Windows.Forms.Button();
-        this.button1 = new System.Windows.Forms.Button();
-        this.SuspendLayout();
-        // 
-        // cancelBtn
-        // 
-        this.cancelBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-        this.cancelBtn.Location = new System.Drawing.Point(463, 298);
-        this.cancelBtn.Name = "cancelBtn";
-        this.cancelBtn.Size = new System.Drawing.Size(75, 23);
-        this.cancelBtn.TabIndex = 8;
-        this.cancelBtn.Text = "Cancel";
-        this.cancelBtn.UseVisualStyleBackColor = true;
-        this.cancelBtn.Click += new System.EventHandler(this.cancelBtn_Click);
-        // 
-        // nextBtn
-        // 
-        this.nextBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-        this.nextBtn.Location = new System.Drawing.Point(367, 298);
-        this.nextBtn.Name = "nextBtn";
-        this.nextBtn.Size = new System.Drawing.Size(75, 23);
-        this.nextBtn.TabIndex = 7;
-        this.nextBtn.Text = "Next";
-        this.nextBtn.UseVisualStyleBackColor = true;
-        this.nextBtn.Click += new System.EventHandler(this.nextBtn_Click);
-        // 
-        // backBtn
-        // 
-        this.backBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-        this.backBtn.Location = new System.Drawing.Point(286, 298);
-        this.backBtn.Name = "backBtn";
-        this.backBtn.Size = new System.Drawing.Size(75, 23);
-        this.backBtn.TabIndex = 6;
-        this.backBtn.Text = "Back";
-        this.backBtn.UseVisualStyleBackColor = true;
-        this.backBtn.Click += new System.EventHandler(this.backBtn_Click);
-        // 
-        // button1
-        // 
-        this.button1.Location = new System.Drawing.Point(135, 88);
-        this.button1.Name = "button1";
-        this.button1.Size = new System.Drawing.Size(75, 23);
-        this.button1.TabIndex = 9;
-        this.button1.Text = "Test";
-        this.button1.UseVisualStyleBackColor = true;
-        this.button1.Click += new System.EventHandler(this.button1_Click);
-        // 
-        // EmptyDialog
-        // 
-        this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.ClientSize = new System.Drawing.Size(560, 333);
-        this.Controls.Add(this.button1);
-        this.Controls.Add(this.cancelBtn);
-        this.Controls.Add(this.nextBtn);
-        this.Controls.Add(this.backBtn);
-        this.Name = "CustomDialog";
-        this.Text = "Custom CLR Dialog";
-        this.ResumeLayout(false);
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CustomDialog));
+            this.cancelBtn = new System.Windows.Forms.Button();
+            this.nextBtn = new System.Windows.Forms.Button();
+            this.backBtn = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cancelBtn
+            // 
+            this.cancelBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cancelBtn.Location = new System.Drawing.Point(463, 298);
+            this.cancelBtn.Name = "cancelBtn";
+            this.cancelBtn.Size = new System.Drawing.Size(75, 23);
+            this.cancelBtn.TabIndex = 8;
+            this.cancelBtn.Text = "Cancel";
+            this.cancelBtn.UseVisualStyleBackColor = true;
+            this.cancelBtn.Click += new System.EventHandler(this.cancelBtn_Click);
+            // 
+            // nextBtn
+            // 
+            this.nextBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.nextBtn.Location = new System.Drawing.Point(367, 298);
+            this.nextBtn.Name = "nextBtn";
+            this.nextBtn.Size = new System.Drawing.Size(75, 23);
+            this.nextBtn.TabIndex = 7;
+            this.nextBtn.Text = "Next";
+            this.nextBtn.UseVisualStyleBackColor = true;
+            this.nextBtn.Click += new System.EventHandler(this.nextBtn_Click);
+            // 
+            // backBtn
+            // 
+            this.backBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.backBtn.Location = new System.Drawing.Point(286, 298);
+            this.backBtn.Name = "backBtn";
+            this.backBtn.Size = new System.Drawing.Size(75, 23);
+            this.backBtn.TabIndex = 6;
+            this.backBtn.Text = "Back";
+            this.backBtn.UseVisualStyleBackColor = true;
+            this.backBtn.Click += new System.EventHandler(this.backBtn_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(463, 260);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Details...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 146);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(465, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "The Windows Subsystem for Linux is not installed on this machine and is required " +
+    "by this program.";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(560, 131);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 168);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(263, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Please install it by following the instructions in this link: ";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(132, 191);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(293, 13);
+            this.linkLabel1.TabIndex = 13;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "https://docs.microsoft.com/en-us/windows/wsl/install-win10";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 232);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(168, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "And re-run this installer afterwards.";
+            // 
+            // CustomDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(560, 333);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cancelBtn);
+            this.Controls.Add(this.nextBtn);
+            this.Controls.Add(this.backBtn);
+            this.Name = "CustomDialog";
+            this.Text = "Custom CLR Dialog";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
     }
 
@@ -96,4 +157,9 @@
     private System.Windows.Forms.Button nextBtn;
     private System.Windows.Forms.Button backBtn;
     private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.PictureBox pictureBox1;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.LinkLabel linkLabel1;
+    private System.Windows.Forms.Label label3;
 }
